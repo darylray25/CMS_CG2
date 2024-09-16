75c99568dfc14ab096d6a67afe016bb385f0af55
darylray25 <saluta.darylray@gmail.com>
2024-09-16 22:40:35 +0800

    super nice

--- resources/views/articles/newsarticle.blade.php
+++ resources/views/articles/newsarticle.blade.php
@@ -20,13 +20,17 @@
                             @endphp
                             @if (in_array($extension, ['jpg', 'jpeg', 'png', 'gif']))
                                 <!-- Image -->
-                                <img src="{{ asset('storage/media/' . $article->image) }}" class="card-img-top article-media" alt="{{ $article->title }}">
+                                <a href="{{ route('articles.show', $article->id) }}">
+                                    <img src="{{ asset('storage/media/' . $article->image) }}" class="card-img-top article-media" alt="{{ $article->title }}">
+                                </a>
                             @elseif (in_array($extension, ['mp4', 'avi', 'mov', 'mkv']))
                                 <!-- Video -->
-                                <video controls class="card-img-top article-media">
-                                    <source src="{{ asset('storage/media/' . $article->image) }}" type="video/{{ $extension }}">
-                                    Your browser does not support the video tag.
-                                </video>
+                                <a href="{{ route('articles.show', $article->id) }}">
+                                    <video controls class="card-img-top article-media">
+                                        <source src="{{ asset('storage/media/' . $article->image) }}" type="video/{{ $extension }}">
+                                        Your browser does not support the video tag.
+                                    </video>
+                                </a>
                             @endif
                         @endif
 
@@ -34,7 +38,6 @@
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title">{{ $article->title }}</h5>
                             <p class="card-text flex-fill">{{ Str::limit($article->content, 100) }}</p>
-                            <a href="{{ route('articles.show', $article->id) }}" class="btn btn-primary">Read More</a>
                         </div>
 
                         <!-- Article Footer -->
@@ -86,7 +89,7 @@
         padding: 1.5rem;
         display: flex;
         flex-direction: column;
-        height: 160px; /* Adjust this value as needed */
+        height: 120px; /* Adjust this value as needed */
         overflow: hidden;
     }
 
